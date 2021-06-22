e82ab742b270e5802c4438a309dcfed32ebfd9d9
Mike <mike.sierra.7950@gmail.com>
2021-06-22 15:54:12 -0400

    saved new addition

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -9,6 +9,9 @@ namespace communication_log
             Console.WriteLine("Hello Pilot");
 
             Console.WriteLine("Hello Control. Do you copy? Test 2");
+
+            Console.WriteLine("Copy loud and clear. Go ahead.");
+
         }
     }
 }
