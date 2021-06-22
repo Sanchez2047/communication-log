121b84759bd1c44ce0d15f0ce9c2946c081c4043
Mike <mike.sierra.7950@gmail.com>
2021-06-22 11:32:36 -0400

    Initial creation

---
+++ communication-log/communication-log/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace communication_log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter name: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hello {input}");
+
+            
+        }
+    }
+}
